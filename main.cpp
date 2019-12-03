acf92ac8af5c23ee5e7841abbd18853509a8b477
Aidan Coopman <aidan.coopman@yahoo.com>
2019-12-02 19:03:39 -0700

    Cleaned up inlcude files

--- main.cpp
+++ main.cpp
@@ -6,20 +6,10 @@
 // for Programmers by Michael J Donahoo
 
 #include <stdio.h>      /* for printf() and fprintf() */
-#include <sys/socket.h> /* for socket(), bind, and connect() */
-#include <arpa/inet.h>  /* for sockaddr_in and inet_ntoa() */
 #include <stdlib.h>     /* for atoi() and exit() */
 #include <string.h>     /* for memset() */
-#include <unistd.h>     /* for close() and getpid() */
-#include <fcntl.h>      /* for fcntl() */
-#include <sys/file.h>   /* for O_NONBLOCK and FASYNC */
-#include <signal.h>     /* for signal() and SIGALRM */
-#include <errno.h>      /* for errno */
 #include <math.h>
 #include "socket.h"
-
-//--------------
-
 #include "cv.h"
 #include "highgui.h"
 #include "cvplot.h"
